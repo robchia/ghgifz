e24ac9cbbe0cea0e93a7f084b356fd365cc9cf68
Rob Chia <hello@robchia.com>
2017-11-09 00:46:52 -0500

    update gifs

--- scripts/giphy-gifs-from-ids.ts
+++ scripts/giphy-gifs-from-ids.ts
@@ -8,7 +8,11 @@ const ids = JSON.parse(contents).ids;
 
 Giphy.gifs(ids, (json) => {
   const gifsFilePath = './data/gifs.json';
-  fs.writeFile(gifsFilePath, JSON.stringify({ gifs: json.data }, null, 2), (err) => {
+  const versionPath = './data/gifs.version'
+  const timestamp = (new Date()).getTime();
+
+  fs.writeFile(gifsFilePath, JSON.stringify({ version: timestamp, gifs: json.data }, null, 2), (err) => {
     console.log(json.data.length, 'gifs -> data/gifs.json');
+    fs.writeFile(versionPath, timestamp);
   });
 });
